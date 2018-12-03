27ef0092e080cad33973142e97128be36441cd30
Cathal <cathalmccurdy24@gmail.com>
2018-12-03 17:20:02 +0000

    fixed some errors

--- Click.py
+++ Click.py
@@ -2,21 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.IN)
-
-kill = input('Press E to End')
+GPIO.setup(7,GPIO.IN,GPIO.PUD_UP)
+#this sets up the pins
 
 while True:
     if GPIO.input(7) == 1:
         print('Ouch!')
     else:
         print('Push me!')
+    #this asks if the button has been pressed
 
-    GPIO.cleanup()
 
-    if kill = 'e':
-        sys.exit(0)
-    else:
-    #this kills the program
 
+   
     
